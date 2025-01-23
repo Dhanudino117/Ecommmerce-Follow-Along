5d4f6d7965c801cb301fa3592af9c7213eb39fb1
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-01-23 14:51:20 +0530

    MileStone 8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
-import './App.css'
-import LoginPage from './pages/LoginPage'
-import SignUpPage  from './pages/SignUpPage'
-import {Routes,Route,BrowserRouter} from 'react-router-dom'
+import "./App.css";
+import Home from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/signup" element={<SignUpPage/>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
