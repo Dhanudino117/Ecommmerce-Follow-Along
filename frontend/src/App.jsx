d06b8a604817b67f2a638bedddf9460708d98c83
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-02-10 14:38:21 +0530

    Milestone 15

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,29 @@
-import './App.css'
-import LoginPage from './pages/LoginPage'
-import SignUpPage  from './pages/SignUpPage'
-import HomePage from './pages/HomePage'
-import CreatePage from './pages/createPage'
-import MyProducts from './pages/myProduct'
-import {Routes,Route,BrowserRouter} from 'react-router-dom'
-
+import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/createPage";
+import MyProducts from "./pages/myProduct";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import CreateProduct from "./pages/createPage";
+import Cart from "./pages/cart";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/signup" element={<SignUpPage/>}/>
-        <Route path='/create-product' element={<CreatePage/>}/>
-        <Route path="/my-products" element={<MyProducts/>} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/create-product" element={<CreatePage />} />
+          <Route path="/my-products" element={<MyProducts />} />
+          <Route path="/create-product/:id" element={<CreateProduct />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
