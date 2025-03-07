b11ba3b8c998804dfdfd28ed030082e36474c250
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-03-07 09:50:49 +0530

    Milestone 26

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,9 @@ import MyProducts from './pages/myProducts';
 import Cart from './pages/cart';
 import ProductDetails from './pages/ProductDetails'
 import Profile from "./pages/profile"
-
+import CreateAddress from './pages/createAddress';
+import SelectAddress from './pages/Selectaddress';
+import OrderConfirmation from './pages/orderConfirmation';
 const App = () => {
   return (
     <BrowserRouter>
@@ -23,6 +25,9 @@ const App = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/create-address" element={<CreateAddress />} />     
+        <Route path="/select-address" element={<SelectAddress />} />   
+        <Route path="/order-confirmation" element={<OrderConfirmation />} />
     </Routes>
     </BrowserRouter>
     
